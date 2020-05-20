e453935ca8b08b03226cca2f173e5648a4ca7764
Fernanda Stella <38155569+FehStella@users.noreply.github.com>
2020-05-20 19:50:29 -0300

    Subindo codigo

---
+++ ac5.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+class listaRepositorios():
+
+    def __init__(self, usuario):
+        self._usuario = usuario
+
+    def requisicao_api(self):
+        resposta = requests.get(f'https://api.github.com/users/{self._usuario}/repos')
+        if resposta.status_code == 200:
+            return resposta.json()
+        else:
+            return resposta.status_code
+    
+    def imprime_repositorios(self):
+        dados_api = self.requisicao_api()
+        if type(dados_api) is not int:
+            for i in range(len(dados_api)):
+                print(dados_api[i]['name'])
+        else:
+            print(dados_api)
+
+repositorios = listaRepositorios('xxxyyyzzzab')
+repositorios.imprime_repositorios()
